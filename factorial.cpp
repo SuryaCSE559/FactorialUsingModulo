c8545b160d8cc854835b2663b2617486b998c171
SuryaCSE559 <84056644+SuryaCSE559@users.noreply.github.com>
2021-05-17 11:09:24 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,mod=1e9+7;
+	cin>>t;
+	while(t--)
+	{
+		long long int n,fact=1;
+	    cin>>n;
+	    if(n==0 || n==1)
+        {
+			cout<<"1"<<endl;
+			continue;
+		}
+		n%=mod;
+	    for(int i=2;i<=n;i++)
+	    {
+	 	   fact=(fact*i)%mod;
+	    }
+	    cout<<fact<<endl;
+	}	
+}
